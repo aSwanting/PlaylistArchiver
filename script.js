7fc58f7857fa135899543f9f4c6357b0eff4f5fe
aSwanting <gab.damico@gmail.com>
2025-01-09 13:12:26 +0100

    refactor, style and layout tweaks

--- script.js
+++ script.js
@@ -1,81 +1,108 @@
-async function addEventListeners() {
-  const panelHeader = document.getElementById("panel-header");
-  const panelBody = document.getElementById("panel-body");
-  const output = document.getElementById("output");
-  const apiInput = document.getElementById("api-input");
-  const idInput = document.getElementById("id-input");
-  const apiBtn = document.getElementById("save-api");
-  const idBtn = document.getElementById("save-id");
-  const fetchBtn = document.getElementById("fetch-info");
-  const params = document.getElementById("parameters");
+function getElements() {
+  return {
+    panelHeader: document.getElementById("panel-header"),
+    panelBody: document.getElementById("panel-body"),
+    output: document.getElementById("output"),
+    apiInput: document.getElementById("api-input"),
+    idInput: document.getElementById("id-input"),
+    apiBtn: document.getElementById("save-api"),
+    idBtn: document.getElementById("save-id"),
+    fetchBtn: document.getElementById("fetch-info"),
+    params: document.getElementById("parameters"),
+  };
+}
 
-  let api = localStorage.getItem("api") || "";
-  let id = localStorage.getItem("id") || "";
-  let secretKey = "";
+function prepareParamsForm(fetchBtn) {
+  const api = localStorage.getItem("api") || "";
+  const id = localStorage.getItem("id") || "";
   fetchBtn.disabled = !(api && id);
+  return { api, id };
+}
 
+function updateParamsDisplay(params, api, id) {
+  let secretKey = "";
+  params.style.display = api || id ? "flex" : "none";
+  if (api.length) {
+    secretKey =
+      api.slice(0, 3) +
+      "******************" +
+      api.slice(api.length - 3, api.length);
+  }
+  params.innerHTML = `
+      <p>Api Key: <span>${secretKey}</span></p>
+      <p>Playlist ID: <span>${id}</span></p>
+    `;
+}
+
+function updateDetailsText(detailsText, data) {
+  detailsText.innerHTML = `
+    <p>Title: <span>${data.info.title}</span></p>
+    <p>Channel: <span>${data.info.channel}</span></p>
+    <p>Published: <span>${data.info.date}</span></p>
+    <p>Total: <span>${data.info.total}</span> 
+    | Deleted: <span>${data.info.deleted}</span> 
+    | Private: <span>${data.info.private}</span> 
+    ${
+      data.info.snapshot ? `| Snapshot: <span>${data.info.snapshot}</span>` : ""
+    }
+    </p><a href="${data.info.url}" target="_blank">View playlist on Youtube</a>
+    `;
+}
+
+async function loadLoader() {
   const loader = document.querySelector(".loader");
   await fetch("./audio.svg")
     .then((response) => response.text())
     .then((data) => (loader.innerHTML = data));
+  return loader;
+}
 
-  function updateParamsDisplay() {
-    params.style.display = api || id ? "flex" : "none";
-    if (api.length) {
-      secretKey =
-        api.slice(0, 3) +
-        "******************" +
-        api.slice(api.length - 3, api.length);
-    }
-    params.innerHTML = `
-      <p>Api Key: <span>${secretKey}</span></p>
-      <p>Playlist ID: <span>${id}</span></p>
-    `;
+async function togglePanel(panelHeader, panelBody) {
+  const h = panelBody.scrollHeight;
+  const p = 16;
+  if (panelBody.style.height == "0px") {
+    panelHeader.classList.remove("closed");
+    panelBody.style.height = h + p * 2 + "px";
+    panelBody.style.paddingBlock = p + "px";
+    await new Promise((resolve) => {
+      panelBody.addEventListener("transitionend", resolve, { once: true });
+    });
+    panelBody.style.height = "auto";
+  } else {
+    panelHeader.classList.add("closed");
+    panelBody.style.height = h + "px";
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    panelBody.style.height = "0px";
+    panelBody.style.paddingBlock = 0 + "px";
   }
+}
 
-  function saveApi(api) {
-    if (localStorage.getItem("api") && localStorage.getItem("api") !== api) {
-      if (!confirm("Overwrite API?")) return false;
-    }
-    localStorage.setItem("api", api);
-    apiInput.value = "";
-    updateParamsDisplay();
-    return true;
+function saveItem(type, value, inputElement, params, api, id) {
+  const existing = localStorage.getItem(type);
+  if (existing && existing !== value) {
+    if (!confirm(`Overwrite ${type.toUpperCase()}?`)) return false;
   }
+  localStorage.setItem(type, value);
+  inputElement.value = "";
+  updateParamsDisplay(params, api, id);
+  return true;
+}
 
-  function saveId(id) {
-    if (localStorage.getItem("id") && localStorage.getItem("id") !== id) {
-      if (!confirm("Overwrite ID?")) return false;
-    }
-    localStorage.setItem("id", id);
-    idInput.value = "";
-    updateParamsDisplay();
-    return true;
-  }
+function addEventListeners(elements, loader, api, id) {
+  const {
+    panelHeader,
+    panelBody,
+    apiBtn,
+    apiInput,
+    idBtn,
+    idInput,
+    fetchBtn,
+    params,
+  } = elements;
 
-  async function togglePanel() {
-    const h = panelBody.scrollHeight;
-    const p = 16;
-    if (panelBody.style.height == "0px") {
-      panelHeader.classList.remove("closed");
-      panelBody.style.height = h + p * 2 + "px";
-      panelBody.style.paddingBlock = p + "px";
-      await new Promise((resolve) => {
-        panelBody.addEventListener("transitionend", resolve, { once: true });
-      });
-      panelBody.style.height = "auto";
-    } else {
-      panelHeader.classList.add("closed");
-      panelBody.style.height = h + "px";
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      panelBody.style.height = "0px";
-      panelBody.style.paddingBlock = 0 + "px";
-    }
-  }
-
-  updateParamsDisplay();
-
-  panelHeader.addEventListener("click", togglePanel);
+  panelHeader.addEventListener("click", () => {
+    togglePanel(panelHeader, panelBody);
+  });
 
   apiInput.addEventListener("input", () => {
     api = apiInput.value.trim();
@@ -91,7 +118,7 @@ async function addEventListeners() {
 
   apiBtn.addEventListener("click", () => {
     if (api.length >= 8) {
-      saveApi(api);
+      saveItem("api", api, apiInput, params, api, id);
     } else {
       alert("Invalid API Key");
     }
@@ -99,35 +126,33 @@ async function addEventListeners() {
 
   idBtn.addEventListener("click", () => {
     if (id.length >= 8) {
-      saveId(id);
+      saveItem("id", id, idInput, params, api, id);
     } else {
       alert("Invalid Playlist ID");
     }
   });
 
   fetchBtn.addEventListener("click", async () => {
-    if (saveApi(api) && saveId(id)) {
+    if (
+      saveItem("api", api, apiInput, params, api, id) &&
+      saveItem("id", id, idInput, params, api, id)
+    ) {
       output.innerHTML = "";
-      togglePanel();
+      togglePanel(panelHeader, panelBody);
       loader.classList.add("visible");
-      const localData = JSON.parse(localStorage.getItem("playlistData"));
-      if (localData && localData.info.id == id) {
-        printItems(localData);
-      } else {
-        const data = await fetchPlaylist(api, id);
-        const items = await fetchPlaylistItems(api, id);
-        const formattedData = formatPlaylistData(data, items);
-        loader.classList.remove("visible");
-        printItems(formattedData);
-      }
+
+      const data = await fetchPlaylist(api, id);
+      const items = await fetchPlaylistItems(api, id);
+      const formattedData = formatPlaylistData(data, items);
+      loader.classList.remove("visible");
+      printItems(formattedData);
     }
   });
 }
 
 async function fetchPlaylist(key, id) {
   const url = "https://www.googleapis.com/youtube/v3/playlists?";
-  const part = "snippet";
-  const params = new URLSearchParams({ key, id, part });
+  const params = new URLSearchParams({ key, id, part: "snippet" });
   const apiUrl = url + params;
   const response = await fetch(apiUrl);
   const data = await response.json();
@@ -137,9 +162,12 @@ async function fetchPlaylist(key, id) {
 
 async function fetchPlaylistItems(key, playlistId) {
   const url = "https://www.googleapis.com/youtube/v3/playlistItems?";
-  const part = "snippet";
-  const maxResults = 50;
-  const params = new URLSearchParams({ key, playlistId, part, maxResults });
+  const params = new URLSearchParams({
+    key,
+    playlistId,
+    part: "snippet",
+    maxResults: 50,
+  });
 
   let items = [];
   let next = "";
@@ -214,16 +242,8 @@ async function printItems(data) {
   output.appendChild(gridWrapper);
   gridWrapper.appendChild(gridWrapperScroll);
 
-  detailsText.innerHTML = `
-  <p>Title: <span>${data.info.title}</span></p>
-  <p>Channel: <span>${data.info.channel}</span></p>
-  <p>Published: <span>${data.info.date}</span></p>
-  <p>Total: <span>${data.info.total}</span> 
-  | Deleted: <span>${data.info.deleted}</span> 
-  | Private: <span>${data.info.private}</span> 
-  ${data.info.snapshot ? `| Snapshot: <span>${data.info.snapshot}</span>` : ""}
-  </p><a href="${data.info.url}" target="_blank">View playlist on Youtube</a>
-  `;
+  updateDetailsText(detailsText, data);
+
   findMissingBtn.innerHTML = "Search for missing playlist items";
 
   for (const item of data.items) {
@@ -235,6 +255,9 @@ async function printItems(data) {
 
   findMissingBtn.addEventListener("click", async () => {
     findMissingBtn.disabled = true;
+    findMissingBtn.innerHTML = "Searching...";
+    findMissingBtn.classList.add("searching");
+
     const gridItems = document.querySelectorAll(".grid-item");
     const localData = JSON.parse(localStorage.getItem("playlistData"));
     const info = localData ? localData.info : data.info;
@@ -242,7 +265,6 @@ async function printItems(data) {
     const missingItemsCount = info.deleted + info.private;
 
     let checkedCount = 0;
-    let snapshotCount = 0;
     let failedCount = 0;
 
     info.deleted = 0;
@@ -251,26 +273,35 @@ async function printItems(data) {
 
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
+      const gridItem = gridItems[i];
       if (item.class == "deleted" || item.class == "private") {
+        gridItem.className = `grid-item checking`;
+        gridItem.innerHTML = "<a><p>Checking...</p></a>";
         if (!item.wayback) {
-          console.log(`Checking ${++checkedCount} of ${missingItemsCount}`);
-          const proxyUrl = "https://api.codetabs.com/v1/proxy?quest=";
-          const cdxUrl = "https://web.archive.org/cdx/search/cdx?";
-          const url = item.url;
-          const output = "json";
-          const sort = "ascending";
-          const limit = 1;
-          const fl = "timestamp,original,statuscode";
-          const params = new URLSearchParams({ url, output, sort, limit, fl });
-          const apiUrl = proxyUrl + encodeURIComponent(cdxUrl + params);
-          const response = await fetch(apiUrl);
-          if (response.ok) {
+          try {
+            console.log(`Checking ${++checkedCount} of ${missingItemsCount}`);
+            const proxyUrl = "https://api.codetabs.com/v1/proxy?quest=";
+            const cdxUrl = "https://web.archive.org/cdx/search/cdx?";
+            const params = new URLSearchParams({
+              url: item.url,
+              output: "json",
+              sort: "ascending",
+              limit: 1,
+              fl: "timestamp,original,statuscode",
+            });
+            const apiUrl = proxyUrl + encodeURIComponent(cdxUrl + params);
+
+            const response = await fetch(apiUrl);
+
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
             const resData = await response.json();
             item.wayback = resData;
-            console.log(resData);
+
             if (resData.length) {
-              snapshotCount++;
-              const gridItem = gridItems[i];
+              console.log("Snapshot found:", resData);
               const timestamp = resData[1][0];
               item.title = "Snapshot";
               item.class = "snapshot";
@@ -278,59 +309,41 @@ async function printItems(data) {
               item.wbTitle = await getTitleFromLink(proxyUrl + item.wbUrl);
               gridItem.className = "grid-item snapshot";
               gridItem.innerHTML = `
-            <a href="${item.wbUrl}" target="_blank">
-            <p>${item.title}<br>${item.wbTitle}</p>
-              </a>
-            `;
-              console.log(item);
+                <a href="${item.wbUrl}" target="_blank">
+                  <p>${item.wbTitle}</p>
+                </a>
+              `;
+            } else {
+              gridItem.className = `grid-item ${item.class}`;
+              gridItem.innerHTML = `<a href="${item.url}" target="_blank"><p>${item.title}</p></a>`;
+              console.log("No snapshot found");
             }
-          } else {
+          } catch (error) {
+            gridItem.className = `grid-item ${item.class}`;
+            gridItem.innerHTML = `<a href="${item.url}" target="_blank"><p>${item.title}</p></a>`;
+            console.error(`Error processing item ${i}:`, error);
             failedCount++;
           }
         }
       }
+
       if (item.class === "deleted") info.deleted++;
       if (item.class === "private") info.private++;
       if (item.class === "snapshot") info.snapshot++;
     }
-    console.log("Search completed");
-    console.log("checked: ", checkedCount);
-    console.log("snapshots found: ", snapshotCount);
-    console.log("failed scans: ", failedCount);
-    console.log("deleted: ", info.deleted);
-    console.log("private: ", info.private);
-    console.log("snapshot: ", info.snapshot);
+
+    logResults(checkedCount, failedCount, info);
 
     localStorage.setItem("playlistData", JSON.stringify({ info, items }));
     console.log("Data saved to local storage");
-    findMissingBtn.disabled = false;
 
-    detailsText.innerHTML = `
-    <p>Title: <span>${info.title}</span></p>
-    <p>Channel: <span>${info.channel}</span></p>
-    <p>Published: <span>${info.date}</span></p>
-    <p>Total: <span>${info.total}</span> 
-    | Deleted: <span>${info.deleted}</span> 
-    | Private: <span>${info.private}</span> 
-    ${info.snapshot ? `| Snapshot: <span>${info.snapshot}</span>` : ""}
-    </p><a href="${info.url}" target="_blank">View playlist on Youtube</a>
-    `;
+    findMissingBtn.disabled = false;
+    findMissingBtn.innerHTML = "Search for missing playlist items";
+    findMissingBtn.classList.remove("searching");
+
+    updateDetailsText(detailsText, data);
   });
-  async function getTitleFromLink(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const text = await response.text();
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(text, "text/html");
-      return doc.title;
-    } catch (error) {
-      console.error("Error fetching title:", error);
-      return null;
-    }
-  }
+
   requestAnimationFrame(async () => {
     details.style.opacity = 1;
     details.style.transform = "translateY(0px)";
@@ -340,8 +353,39 @@ async function printItems(data) {
   });
 }
 
+async function getTitleFromLink(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const text = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/html");
+    return doc.title && doc.title !== "YouTube" ? doc.title : "Unknown Title";
+  } catch (error) {
+    console.error("Error fetching title:", error);
+    return "Unknown Title";
+  }
+}
+
+function logResults(checkedCount, failedCount, info) {
+  console.log("Search completed");
+  console.log("checked: ", checkedCount);
+  console.log("failed scans: ", failedCount);
+  console.log("deleted: ", info.deleted);
+  console.log("private: ", info.private);
+  console.log("snapshot: ", info.snapshot);
+}
+
 async function run() {
-  addEventListeners();
+  const elements = getElements();
+  const { api, id } = prepareParamsForm(elements.fetchBtn);
+  const loader = await loadLoader();
+  updateParamsDisplay(elements.params, api, id);
+  addEventListeners(elements, loader, api, id);
+  const localData = JSON.parse(localStorage.getItem("playlistData"));
+  if (localData && localData.info.id == id) printItems(localData);
 }
 
 run();
